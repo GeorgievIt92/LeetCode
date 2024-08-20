52f50466ff8e8fb1670886210b4f5189cfe0fa96
GeorgievIt92 <georgievit92@gmail.com>
2024-08-20 19:35:17 +0300

    Added remove elements

---
+++ RemoveElement/RemoveElement/Program.cs
@@ -0,0 +1,21 @@
+﻿var nums = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+var val = 2;
+var expectedNums = new int[nums.Length];
+
+var k = 0;
+
+var index = 0;
+
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[i] != val)
+    {
+        k++;
+        expectedNums[index] = nums[i];
+        index++;
+    }
+}
+
+Console.WriteLine(k);
+
+Console.WriteLine(string.Join(", ", expectedNums.Take(index)));
